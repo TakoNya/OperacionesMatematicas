c2b59d1e1f209ec618f1338aaaa4c5b533ae02ef
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-25 22:00:59 -0300

    OperacionesMatemáticas

---
+++ src/operacionesapp/OperacionesApp.java
@@ -0,0 +1,22 @@
+package operacionesapp;
+
+public class OperacionesApp {
+
+       public static void main(String[] args) {
+       //Declaramos las variables
+       int num1 = 10;
+       int num2 = 5;
+       
+       /*Realizamos las operaciones.
+        *También lo podemos guardar el resultado en variables. */
+       
+       System.out.println("El resultado de la suma es " + (num1 + num2));
+       System.out.println("El resultado de la resta es " + (num1- num2));
+       System.out.println("El resultado de la multiplicación es " + (num1 * num2));
+       System.out.println("El resultado de la división es " + (num1 / num2));
+       System.out.println("El resto de la división es " + (num1%num2));
+       
+       }
+       
+    
+}
